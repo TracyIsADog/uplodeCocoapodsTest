78bb799be79f19e439056888c67707e207d454f3
admin <303912095@qq.com>
2018-01-10 17:44:09 +0800

    first commit

---
+++ HelloDemo/HelloDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloDemo
+//
+//  Created by admin on 2018/1/10.
+//  Copyright © 2018年 sxd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloDemo/HelloDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloDemo
+//
+//  Created by admin on 2018/1/10.
+//  Copyright © 2018年 sxd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
