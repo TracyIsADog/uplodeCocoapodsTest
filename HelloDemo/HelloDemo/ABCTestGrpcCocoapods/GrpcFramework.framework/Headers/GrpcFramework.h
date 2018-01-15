6c1aedb4fc7716d4f06652b0bd02542c4b4dbc0f
admin <303912095@qq.com>
2018-01-15 10:41:33 +0800

    1.5

---
+++ HelloDemo/HelloDemo/ABCTestGrpcCocoapods/GrpcFramework.framework/Headers/GrpcFramework.h
@@ -0,0 +1,19 @@
+//
+//  GrpcFramework.h
+//  GrpcFramework
+//
+//  Created by admin on 2018/1/13.
+//  Copyright © 2018年 sxd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GrpcFramework.
+FOUNDATION_EXPORT double GrpcFrameworkVersionNumber;
+
+//! Project version string for GrpcFramework.
+FOUNDATION_EXPORT const unsigned char GrpcFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GrpcFramework/PublicHeader.h>
+
+
